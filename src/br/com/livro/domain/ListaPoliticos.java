9033f98c33eb6fd03044ed22386644bd5c795dc9
GuhSntLean <guleandro.66@gmail.com>
2021-01-06 12:26:02 -0300

    Projeto politico(teste)

---
+++ src/br/com/livro/domain/ListaPoliticos.java
@@ -0,0 +1,24 @@
+package br.com.livro.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="politicos")
+public class ListaPoliticos implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private List<Politico> politicos;
+	@XmlElement(name="politico")
+	public List<Politico> getPoliticos(){
+		return politicos;
+	}
+	public void setPoliticos(List<Politico> politicos) {
+		this.politicos = politicos;
+	}
+	@Override
+	public String toString() {
+		return "ListaPoliticos [politicos="+ politicos+"]";
+	}
+}
